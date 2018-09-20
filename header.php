94e0cbaca450131882cb246d0bc08100d738d410
[u]nkstd <elecktro2005@gmail.com>
2018-09-20 17:29:06 +0300

    123

--- header.php
+++ header.php
@@ -23,14 +23,14 @@
     <meta name="theme-color" content="#ffffff">
 	<!-- Meta Info -->
     <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<meta name="yandex-verification" content="92ab6f768f13768d">
-	<meta name="google" content="nositelinkssearchbox">
+	<meta name="yandex-verification" content="92ab6f768f13768d" />
+	<meta name="google" content="nositelinkssearchbox" />
 	<base href="//tattoo-laboratory.ru/">
-	<meta name="description" content="'Tattoo Laboratory' - это частная творческая студия проектирования и создания татуировок. В планы студии входит создание интересных работ с художественной стороны и уникальных с технической, внедрение новых приемов цветопередачи и гармоничное расположение работы в пластике тела">
+	<meta name="description" content="'Тату лаборатория' - это частная творческая студия проектирования и создания татуировок. В планы студии входит создание интересных работ с художественной стороны и уникальных с технической, внедрение новых приемов цветопередачи и гармоничное расположение работы в пластике тела">
 	<meta name="application-name" content="Tattoo Laboratory">
-	<meta name="robots" content="all">
+	<meta name="robots" content="all"/>
 	<!-- Open Graph -->
 	<meta property="og:url" content="http://tattoo-laboratory.ru">
 	<meta property="og:type" content="article">
@@ -45,141 +45,141 @@
     <?php
 
     if(is_page('tattoo')){
-        ?>
-            <style>
-                .main-header {
-                    background: url('<?php bloginfo('template_directory') ?>/public/0590ef9fd12239ee6b34c474c5062fd1.png') no-repeat center;
-                    background-size: 100%;
-                }
-                @media screen and (max-width: 1257px) {
+            ?>
+                <style>
                     .main-header {
-                        background-size: auto 100%;
-                        background-position: -1100px 0;
+                        background: url('<?php bloginfo('template_directory') ?>/public/0590ef9fd12239ee6b34c474c5062fd1.png') no-repeat center;
+                        background-size: 100%;
                     }
-                }
-            </style>
-        <?php
+                    @media screen and (max-width: 1257px) {
+                        .main-header {
+                            background-size: auto 100%;
+                            background-position: -1100px 0;
+                        }
+                    }
+                </style>
+            <?php
     }
     
     if(is_page('sketches')){
-        ?>
-            <style>
-                .main-header {
-                    background: url('<?php bloginfo('template_directory') ?>/public/0461834a7bc525aa46e37003f0ed7d8b.png') no-repeat center;
-                    background-size: 100%;
-                }
-                @media screen and (max-width: 1257px) {
+            ?>
+                <style>
                     .main-header {
-                        background-size: auto 100%;
-                        background-position: -500px 0;
+                        background: url('<?php bloginfo('template_directory') ?>/public/0461834a7bc525aa46e37003f0ed7d8b.png') no-repeat center;
+                        background-size: 100%;
                     }
-                }
-            </style>
-        <?php
+                    @media screen and (max-width: 1257px) {
+                        .main-header {
+                            background-size: auto 100%;
+                            background-position: -500px 0;
+                        }
+                    }
+                </style>
+            <?php
     }
     
     if(is_page('pictures')){
-        ?>
-            <style>
-                .main-header {
-                    background: url('<?php bloginfo('template_directory') ?>/public/eac28b3aaa3e6b221475743acea68a12.png') no-repeat;
-                    background-size: 100%;
-                }
-                @media screen and (max-width: 1257px) {
+            ?>
+                <style>
                     .main-header {
-                        background-size: auto 100%;
-                        background-position: -300px 0;
+                        background: url('<?php bloginfo('template_directory') ?>/public/eac28b3aaa3e6b221475743acea68a12.png') no-repeat;
+                        background-size: 100%;
                     }
-                }
-            </style>
-        <?php
+                    @media screen and (max-width: 1257px) {
+                        .main-header {
+                            background-size: auto 100%;
+                            background-position: -300px 0;
+                        }
+                    }
+                </style>
+            <?php
     }
     
     if(is_page('info')){
-        ?>
-            <style>
-                .main-header {
-                    background: url('<?php bloginfo('template_directory') ?>/public/bafeac6bf1d555508e92662aec8bc964.png') no-repeat;
-                    background-size: 100%;
-                }
-                @media screen and (max-width: 1257px) {
+            ?>
+                <style>
                     .main-header {
-                        background-size: auto 100%;
-                        background-position: -1500px 0;
+                        background: url('<?php bloginfo('template_directory') ?>/public/bafeac6bf1d555508e92662aec8bc964.png') no-repeat;
+                        background-size: 100%;
                     }
-                }
-            </style>
-        <?php
+                    @media screen and (max-width: 1257px) {
+                        .main-header {
+                            background-size: auto 100%;
+                            background-position: -1500px 0;
+                        }
+                    }
+                </style>
+            <?php
     }
     
     if(is_page('contacts')){
-        ?>
-            <style>
-                .main-header {
-                    background: url('<?php bloginfo('template_directory') ?>/public/cdb60039d83ee7adc9e1fa28a20c0c8f.png') no-repeat;
-                    background-position: center;
-                    background-size: 100%;
-                }
-                @media screen and (max-width: 1257px) {
+            ?>
+                <style>
                     .main-header {
+                        background: url('<?php bloginfo('template_directory') ?>/public/cdb60039d83ee7adc9e1fa28a20c0c8f.png') no-repeat;
                         background-position: center;
-                        background-size: auto 100%;
+                        background-size: 100%;
                     }
-                }
-            </style>
-        <?php
+                    @media screen and (max-width: 1257px) {
+                        .main-header {
+                            background-position: center;
+                            background-size: auto 100%;
+                        }
+                    }
+                </style>
+            <?php
     }
     
     if(is_page('tktx')){
-        ?>
-            <style>
-                .main-header {
-                    background: url('<?php bloginfo('template_directory') ?>/public/be2295560e2fc670f79971378ad6a431.png') no-repeat;
-                    background-size: 100%;
-                }
-                @media screen and (max-width: 1257px) {
+            ?>
+                <style>
                     .main-header {
-                        background-position: center;
-                        background-size: auto 100%;
+                        background: url('<?php bloginfo('template_directory') ?>/public/be2295560e2fc670f79971378ad6a431.png') no-repeat;
+                        background-size: 100%;
                     }
-                }
-            </style>
-        <?php
+                    @media screen and (max-width: 1257px) {
+                        .main-header {
+                            background-position: center;
+                            background-size: auto 100%;
+                        }
+                    }
+                </style>
+            <?php
     }
     
     if(is_page('doctor-pro')){
-        ?>
-            <style>
-                .main-header {
-                    background: url('<?php bloginfo('template_directory') ?>/public/794ea9a7d78d28d4362aa24bb580d4c3.png') no-repeat;
-                    background-size: 100%;
-                }
-                @media screen and (max-width: 1257px) {
+            ?>
+                <style>
                     .main-header {
-                        background-position: center;
-                        background-size: auto 100%;
+                        background: url('<?php bloginfo('template_directory') ?>/public/794ea9a7d78d28d4362aa24bb580d4c3.png') no-repeat;
+                        background-size: 100%;
                     }
-                }
-            </style>
-        <?php
+                    @media screen and (max-width: 1257px) {
+                        .main-header {
+                            background-position: center;
+                            background-size: auto 100%;
+                        }
+                    }
+                </style>
+            <?php
     }
     
     if(is_page('dermalize-pro')){
-        ?>
-            <style>
-                .main-header {
-                    background: url('<?php bloginfo('template_directory') ?>/public/5149644f1175dab9f86ce37b50d5be49.png') no-repeat;
-                    -webkit-background-size: 100%;
-                    background-size: 100%;
-                }
-                @media screen and (max-width: 1257px) {
+            ?>
+                <style>
                     .main-header {
-                        background-position: center;
-                        background-size: auto 100%;
+                        background: url('<?php bloginfo('template_directory') ?>/public/5149644f1175dab9f86ce37b50d5be49.png') no-repeat;
+                        -webkit-background-size: 100%;
+                        background-size: 100%;
                     }
-                }
-            </style>
-        <?php
+                    @media screen and (max-width: 1257px) {
+                        .main-header {
+                            background-position: center;
+                            background-size: auto 100%;
+                        }
+                    }
+                </style>
+            <?php
     }
 ?>
 </head>
@@ -189,15 +189,26 @@
         <?php 
             wp_nav_menu( array(
                 'theme_location'  => 'header',
+                'menu'            => '',
+                'container'       => '',
+                'container_class' => '',
+                'container_id'    => '',
+                'menu_class'      => '',
+                'menu_id'         => '',
                 'echo'            => true,
                 'fallback_cb'     => 'wp_page_menu',
+                'before'          => '',
+                'after'           => '',
+                'link_before'     => '',
+                'link_after'      => '',
                 'items_wrap'      => '<ul>%3$s</ul>',
                 'depth'           => 0,
+                'walker'          => '',
             ) );
         ?>
     </div>
 
-    <header class="main-header">
+    <div class="main-header">
         <a href="<?php echo home_url(); ?>"><img src="<?php bloginfo('template_directory') ?>/public/img/logo.svg" class="logo"></a>
         <div class="wrapper-info">
             <div class="info">
@@ -209,13 +220,24 @@
         </div>
         <ul class="nav" id="headerNav">
             <div class="menu">
-                <?php
+                <?php 
                     wp_nav_menu( array(
                         'theme_location'  => 'header',
+                        'menu'            => '',
+                        'container'       => '',
+                        'container_class' => '',
+                        'container_id'    => '',
+                        'menu_class'      => '',
+                        'menu_id'         => '',
                         'echo'            => true,
                         'fallback_cb'     => 'wp_page_menu',
+                        'before'          => '',
+                        'after'           => '',
+                        'link_before'     => '',
+                        'link_after'      => '',
                         'items_wrap'      => '%3$s',
                         'depth'           => 0,
+                        'walker'          => '',
                     ) );
                 ?>
             </div>
@@ -226,4 +248,4 @@
                 <img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" />
             <?php endif; 
         ?>
-    </header>
+    </div>
--- page.php
+++ page.php
@@ -4,22 +4,34 @@
 <div class="content">
 	<?php wp_nav_menu( array(
 		'theme_location'  => 'sidebar',
+		'menu'            => '',
 		'container'       => 'ul',
 		'container_class' => 'sidebar',
+		'container_id'    => '',
 		'menu_class'      => 'sidebar',
+		'menu_id'         => '',
 		'echo'            => true,
 		'fallback_cb'     => 'wp_page_menu',
+		'before'          => '',
+		'after'           => '',
+		'link_before'     => '',
+		'link_after'      => '',
 		'items_wrap'      => '<ul id = "%1$s" class = "%2$s">%3$s</ul>',
 		'depth'           => 0,
+		'walker'          => '',
 	) );
 		if (have_posts()):
 		    while (have_posts()) : the_post(); ?>
 				<h1><?php the_title(); ?></h1>
+<<<<<<< HEAD
 		       	<article>
+=======
+		       	<div class="article">
+>>>>>>> parent of 00b8dde... 1.1.1
 		       		<div class="postContent">
 			       		<?php the_content(); ?>
 		       		</div>
-		       	</article>
+		       	</div>
 		    <?php endwhile;
 		else:
 		    echo '<h1 class="no-content">Запись отстутствуют</h1>';
